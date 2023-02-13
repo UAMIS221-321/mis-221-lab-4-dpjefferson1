3151acdb50f98615768a3c658bbedd73a1f5d428
dpjefferson1 <dpjefferson1@crimson.ua.edu>
2023-02-13 13:13:16 -0600

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Start Main
+DisplayMenu();
+
+//Main
+
+static void DisplayMenu()
+{
+    Console.WriteLine("Press any number to continue");
+    string choice = Console.ReadLine();
+
+    while( choice != "3")
+    {
+        Console.WriteLine("\n1. Full Triangle \n2. Partial Triangle \n3. Exit");
+        choice = Console.ReadLine();
+
+        if (choice == "1")
+        {
+            GetFull();
+        }
+        else if (choice == "2")
+        {
+            GetPartial();
+        }
+        else if (choice == "3")
+        {
+
+        }
+        else {
+            Console.WriteLine("Invalid");
+        }
+    }
+
+    
+}
+ 
+ static void GetFull()
+    {
+        int ran = new Random().Next(3,9);
+
+        for (int i = ran; i > 0; i--)
+        {
+            for (int d = i ; d < ran ; d++)
+            {
+                Console.Write("0\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void GetPartial()
+    {
+        int ran = new Random().Next(3,9);
+
+        for (int i = ran; i > 0; i--)
+        {
+            for (int d = i ; d < ran ; d++)
+            {
+                if (d == 3 || d == 5 || d == 9)
+                {
+                    Console.Write("\t");
+                }
+                Console.Write("0\t");  
+            }
+            Console.WriteLine();
+        }
+    }
